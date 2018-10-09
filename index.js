ccc2639f301d196cc6d8d255c59e11e8883338aa
Graves <igraves@hbs.edu>
2018-10-09 11:23:47 -0400

    changed error

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+express.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: err
+  });
+});
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -10,11 +18,3 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-index.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: err
-  });
-});
