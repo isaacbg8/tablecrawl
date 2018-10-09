ce22414dc6d924500164d522fe662077f79f2765
Graves <igraves@hbs.edu>
2018-10-09 10:45:16 -0400

    changed index.js

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
+const cool = require('cool-ascii-faces')
 const express = require('express')
-const app = express()
-const port = 3000
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-app.get('/', (request, response) => {
-  response.send('Hello from Express!')
-})
-
-app.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-
-  console.log(`server is listening on ${port}`)
-})
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
