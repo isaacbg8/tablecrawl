3ccb6984a6c9a7d33cd72df5ae936843fec0c346
Isaac Graves <isaac@isaacs-mbp.hbs.edu>
2018-10-10 10:26:35 -0400

    added index.html

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/', function (req, res) {
+ res.sendFile(__dirname+'/index.html');
+});
+
+//router.get('/.well-known/acme-challenge/n3QlH7uck3cyfzGSUr1hovvoZWZ14qgQss4otx5Mq1k', (req, res)=>{
+// res.send('n3QlH7uck3cyfzGSUr1hovvoZWZ14qgQss4otx5Mq1k.wEoPBKae2Lz28GBDhP5HTrX4UYota2U5cSqgZv3XMQ8');
+//})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
